3718bedb706c2d3d51cfb0f9c8e58eb038aa81c8
Aymen Bz <aymen@Techlabs-iMac.local>
2019-03-17 23:26:50 +0100

    first commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="todos">
+      <div class="todo" v-for="todo in getTodos" :key="todo.key">
+        <div>{{ todo.title }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapGetters, mapActions } from "vuex";
+  export default {
+    name: "Todos",
+    methods: {
+      ...mapActions(["fetchTodos"])
+    },
+    computed: mapGetters(["getTodos"]),
+
+    created() {
+      this.fetchTodos();
+    }
+  };
+</script>
+
+<style scoped>
+.todos {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.todos .todo {
+  flex: 0 0 25%;
+}
+</style>
